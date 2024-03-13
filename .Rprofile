c5b2f8dd8253d0503f42292f2d7016e34271f4bf
Ozan Ozbeker <ozanozbeker99@outlook.com>
2024-03-13 16:23:38 -0400

    Finalized (for now)

--- .Rprofile
+++ .Rprofile
@@ -1,19 +1,17 @@
-# Use this code section if you want things to enable in an interactive session, like auto-loading certain packages
-if (interactive()) {
-  # require(devtools)
-  require(usethis)
-}
-
-# Options I like, ?options to view more
+# Options I like ----
+# Use ?options to view more
 options(
   # Base
   scipen = 999999,               # A penalty to be applied to turning regular values to scientific notation
   setWidthOnResize = TRUE,       # If set and TRUE, the terminal resize when the terminal pane is resized
   useFancyQuotes = FALSE,        # Turn off fancy quotes
-  warn = 2,                      # Turns warnings into errors. The idea is, if you really want to suppress a warning, silence it with suppressWarnings() and explain why
-  warnPartialMatchAttr = TRUE,   # Warn on partial matching for the attr() function
-  warnPartialMatchDollar = TRUE, # Warn on partial matches for $ operator
-  warnPartialMatchArgs = TRUE,   # Warn on partial matches for function argument names
+  warn = 1,                      # Prints warnings as they happen instead of when the top function ends.
+# warn = 2,                      # Turns warnings into errors. The idea is, if you really want to suppress a warning, silence it with suppressWarnings() and explain why
+  warnPartialMatchArgs = TRUE,   # Warns if partial matching is used in argument matching.
+  warnPartialMatchAttr = TRUE,   # Warns if partial matching is used in extracting attributes via attr.
+  warnPartialMatchDollar = TRUE, # Warns if partial matching is used for extraction by $.
+  prompt = 'R> ',                # This is complete personal preference
+  continue = '+> ',              # This is complete personal preference
 
   # Usethis
   usethis.full_name = 'Ozan Ozbeker',
@@ -21,9 +19,12 @@ options(
   usethis.destdir = 'C:/Users/OzanO/Code'
 )
 
-# ipck, func, & fuzzy are turned off by default. Check out ?utils::rc.settings for more details
-utils::rc.settings(
-  ipck = TRUE, # Auto-complete package names in require() and library() calls
-  func = TRUE, # Enables detection of functions
-  fuzzy = TRUE # Enables fuzzy matching, where close but non-exact matches are considered if exact matches are not found
-)
+# R Completion Options ----
+# `ipck`, `func`, & `fuzzy` are turned off and the rest are turned on by default.
+utils::rc.settings(ipck = TRUE, func = TRUE, fuzzy = TRUE)
+
+# Interactive Code ----
+# Use this code section if you want things to enable in an interactive session, like auto-loading certain packages
+if (interactive()) {
+  "Welcome Ozan, the greatest data scientist to ever live."
+}
--- r-and-rstudio-setup.R
+++ r-and-rstudio-setup.R
@@ -1,21 +1,19 @@
-# Script: RStudio Initial Setup
+# Script: R & RStudio Setup
 # Author: Ozan Ozbeker
 
 # Packages ----
 # These are the packages I use frequently, loosely grouped into the categories below:
 install.packages(c(
   # Meta Packages
-  'tidyverse', # Easily Install and Load the 'Tidyverse' | https://tidyverse.tidyverse.org
-  'tidymodels',
-
+  'tidyverse',  # Easily Install and Load the "Tidyverse" packages | https://tidyverse.tidyverse.org
+  'tidymodels', # Easily Install and Load the "Tidymodels" packages | https://tidymodels.tidymodels.org/
+  
   # Programming & Development
   'devtools', # Tools to Make Developing R Packages Easier | https://devtools.r-lib.org/
   'usethis',  # Automate Package and Project Setup | https://usethis.r-lib.org/
   'keyring',  # Package for accessing OS's credential store | https://keyring.r-lib.org/
-  'fs',       # Provides functions to manage files & directories using tidy principles like tibbles, tidy-paths, etc.
-  'gert',     # Interacts with GitHub as a Git server, using HTTPS or SSH protocol | https://docs.ropensci.org/gert/
-  'gh'       # Interacts with GitHub via its REST API | https://gh.r-lib.org/
-  
+  'fs'        # Provides functions to manage files & directories using tidy principles like tibbles, tidy-paths, etc.
+
   # Import
 
   # Tidy/Transform
@@ -27,44 +25,51 @@ install.packages(c(
   # Communicate
 
   # Database
-
+  
+  # Fun
+  
 ))
 
 # Other packages I'm interested in:
-# 'callr' | https://callr.r-lib.org/
-# 'targets' | https://docs.ropensci.org/targets/
-# 'crew' | (not sure what this does but mentioned in 'targets'), https://wlandau.github.io/crew/
+# 'targets'   # Pipelining Tools in R | https://docs.ropensci.org/targets/
 
 # RStudio IDE Config ----
 # Turns the IDE black:
 devtools::install_github('rileytwo/darkstudio')
 darkstudio::activate() # Must be running with admin privileges, Run & restart RStudio
 
-# Apply preferred RStudio Settings
-if (fs::file_exists('rstudio-prefs.json')) fs::file_copy('rstudio-prefs.json', "C:/Users/OzanO/AppData/Roaming/RStudio/rstudio-prefs.json", overwrite = TRUE) else usethis::edit_rstudio_prefs()
+# Copy my favorite theme into the theme folder
+fs::file_copy(
+  path = 'KISS - OZBEKER.rstheme',
+  new_path = stringr::str_glue("C:/Users/{Sys.info()[['user']]}/AppData/Roaming/RStudio/themes/KISS - OZBEKER.rstheme"),
+  overwrite = TRUE
+)
 
-# Add preferred r.snippets
-if (fs::file_exists('r.snippets')) fs::file_copy('r.snippets', "C:/Users/OzanO/AppData/Roaming/RStudio/snippets/r.snippets", overwrite = TRUE) else usethis::edit_rstudio_snippets()
+# Apply my preferred keybindings
+fs::file_copy(
+  path = 'rstudio_bindings.json',
+  new_path = stringr::str_glue("C:/Users/{Sys.info()[['user']]}/AppData/Roaming/RStudio/keybindings/rstudio_bindings.json"),
+  overwrite = TRUE
+)
 
-# Add preferred .Rprofile config
-if (fs::file_exists('.Rprofile')) fs::file_copy('.Rprofile', "C:/Users/OzanO/Documents/.Rprofile", overwrite = TRUE) else usethis::edit_r_profile()
+# Apply my preferred RStudio Settings
+fs::file_copy(
+  path = 'rstudio-prefs.json', 
+  new_path = stringr::str_glue("C:/Users/{Sys.info()[['user']]}/AppData/Roaming/RStudio/rstudio-prefs.json"), 
+  overwrite = TRUE
+)
 
-# Git Setup ----
-# Install https://git-for-windows.github.io/
+# Apply my preferred R Snippets
+fs::file_copy(
+  path = 'r.snippets', 
+  new_path = stringr::str_glue("C:/Users/{Sys.info()[['user']]}/AppData/Roaming/RStudio/snippets/r.snippets"), 
+  overwrite = TRUE
+)
 
-# Initial Check
-usethis::git_sitrep()
-usethis::gh_token_help()
+# Apply my preferred .Rprofile config
+fs::file_copy(
+  path = '.Rprofile', 
+  new_path = stringr::str_glue("C:/Users/{Sys.info()[['user']]}/Documents/.Rprofile"), 
+  overwrite = TRUE
+)
 
-# Config as needed
-usethis::use_git_config(user.name = 'ozanozbeker', user.email = 'ozanozbeker99@outlook.com')
-usethis::git_vaccinate()
-usethis::git_default_branch_configure(name = 'main')
-usethis::create_github_token(description = 'OZAN-XPS17') # Edit for taste
-gitcreds::gitcreds_set()
-gh::gh_whoami()
-keyring::key_set_with_value(service = 'github', username = 'PAT: OZAN-XPS17', password = NA) # Put PAT here
-keyring::key_list()
-
-# Final Check
-usethis::git_sitrep()
