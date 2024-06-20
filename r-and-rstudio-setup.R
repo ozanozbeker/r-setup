a1449c4b1d0a58682b3ac595967c9185362ff0e4
Ozan Ozbeker <ozanozbeker99@outlook.com>
2024-06-20 02:15:34 -0400

    Update r-and-rstudio-setup.R

--- r-and-rstudio-setup.R
+++ r-and-rstudio-setup.R
@@ -5,18 +5,21 @@ install.packages(c(
   'tidymodels', # Easily Install and Load the "Tidymodels" packages | https://tidymodels.tidymodels.org/
 
   # Programming & Development
-  'purrr',    # Functional Programming Tools | https://purrr.tidyverse.org/
-  'forcats',  # Tools for Working with Categorical Variables (Factors) | https://forcats.tidyverse.org/
-  'keyring',  # Package for accessing OS's credential store | https://keyring.r-lib.org/
-  'fs',       # Cross-Platform File System Operations Based on 'libuv' | https://fs.r-lib.org/
-  'renv',     # Project Environments | https://rstudio.github.io/renv/index.html
+  'purrr',     # Functional Programming Tools | https://purrr.tidyverse.org/
+  'forcats',   # Tools for Working with Categorical Variables (Factors) | https://forcats.tidyverse.org/
+  'keyring',   # Package for accessing OS's credential store | https://keyring.r-lib.org/
+  'fs',        # Cross-Platform File System Operations Based on 'libuv' | https://fs.r-lib.org/
+  'renv',      # Project Environments | https://rstudio.github.io/renv/index.html
+  'pins',      # | https://pins.rstudio.com/
+  'plumber',   # | https://www.rplumber.io/
+  'vetiver',   # | https://rstudio.github.io/vetiver-r/index.html
 
   # Import
-  'readr',    # Read Rectangular Data | https://readr.tidyverse.org/
-  'httr2',    # Perform HTTP Requests and Process the Responses | https://httr2.r-lib.org/
-  'readxl',   # Read Excel Files | https://readxl.tidyverse.org/
-  'rvest',    # Easily Harvest (Scrape) Web Pages | https://rvest.tidyverse.org/
-  'jsonlite', # A Simple and Robust JSON Parser and Generator for R | https://jeroen.r-universe.dev/jsonlite
+  'readr',     # Read Rectangular Data | https://readr.tidyverse.org/
+  'httr2',     # Perform HTTP Requests and Process the Responses | https://httr2.r-lib.org/
+  'readxl',    # Read Excel Files | https://readxl.tidyverse.org/
+  'rvest',     # Easily Harvest (Scrape) Web Pages | https://rvest.tidyverse.org/
+  'jsonlite',  # A Simple and Robust JSON Parser and Generator for R | https://jeroen.r-universe.dev/jsonlite
 
   # Tidy/Transform
   'dplyr',     # A Grammar of Data Manipulation | https://dplyr.tidyverse.org/
@@ -28,6 +31,7 @@ install.packages(c(
 
   # Visualize
   'ggplot2',   # Create Elegant Data Visualizations Using the Grammar of Graphics | https://ggplot2.tidyverse.org/
+  'marquee',   # | https://marquee.r-lib.org/index.html
   'gt',        # Easily Create Presentation-Ready Display Tables | https://gt.rstudio.com/
   'skimr',     # Compart and Flexible Summaries of Data | https://docs.ropensci.org/skimr/
 
@@ -44,14 +48,15 @@ install.packages(c(
   'corrr',     # Correlations in R | https://corrr.tidymodels.org/
 
   # Communicate
-  'quarto', # R Interface to 'Quarto' Markdown Publishing System
+  'quarto',    # R Interface to 'Quarto' Markdown Publishing System
 
   # Database
-  'DBI',     # R Database Interface | https://dbi.r-dbi.org/index.html
-  'odbc',    # Connect to ODBC Compatible Databases (using the {DBI} Interface) | https://odbc.r-dbi.org/
-  'dbplyr',  # A {dplyr} Back End for Databases | https://dbplyr.tidyverse.org/
-  'duckdb',  # {DBI} Package for the DuckDB Database Management System | https://r.duckdb.org/
-  'duckplyr' # A DuckDB-backed Version of {dplyr} | https://duckdblabs.github.io/duckplyr/
+  'DBI',       # R Database Interface | https://dbi.r-dbi.org/index.html
+  'odbc',      # Connect to ODBC Compatible Databases (using the {DBI} Interface) | https://odbc.r-dbi.org/
+  'RPostgres', 
+  'dbplyr',    # A {dplyr} Back End for Databases | https://dbplyr.tidyverse.org/
+  'duckdb',    # {DBI} Package for the DuckDB Database Management System | https://r.duckdb.org/
+  'duckplyr'   # A DuckDB-backed Version of {dplyr} | https://duckdblabs.github.io/duckplyr/
 ))
 
 install.packages(
@@ -72,16 +77,16 @@ rsthemes::install_rsthemes()
 # RStudio Config ----
 purrr::walk(
   .x = c('themes', 'keybindings', 'snippets'),
-  .f = \(directory) stringr::str_c("C:/Users/", {Sys.info()[['user']]}, "/AppData/Roaming/RStudio/", directory) |> fs::dir_create()
+  .f = \(directory) stringr::str_c(Sys.getenv("APPDATA"), "/RStudio/", directory) |> fs::dir_create()
 )
 
 purrr::pwalk(
   .l = tibble::tribble(
     ~file,                   ~destination,
-    'rstudio_bindings.json',  stringr::str_glue("C:/Users/{Sys.info()[['user']]}/AppData/Roaming/RStudio/keybindings/rstudio_bindings.json"),
-    'addins.json',            stringr::str_glue("C:/Users/{Sys.info()[['user']]}/AppData/Roaming/RStudio/keybindings/addins.json"),
-    'r.snippets',             stringr::str_glue("C:/Users/{Sys.info()[['user']]}/AppData/Roaming/RStudio/snippets/r.snippets"),
-    'rstudio-prefs.json',     stringr::str_glue("C:/Users/{Sys.info()[['user']]}/AppData/Roaming/RStudio/rstudio-prefs.json"),
-    '.Rprofile',              stringr::str_glue("C:/Users/{Sys.info()[['user']]}/Documents/.Rprofile")),
+    'rstudio_bindings.json', paste0(Sys.getenv("APPDATA"), "/RStudio/keybindings/rstudio_bindings.json"),
+    'addins.json',           paste0(Sys.getenv("APPDATA"), "/RStudio/keybindings/addins.json"),
+    'r.snippets',            paste0(Sys.getenv("APPDATA"), "/RStudio/snippets/r.snippets"),
+    'rstudio-prefs.json',    paste0(Sys.getenv("APPDATA"), "/RStudio/rstudio-prefs.json"),
+    '.Rprofile',             paste0(Sys.getenv("R_USER"), "/Documents/.Rprofile")),
   .f = \(file, destination) fs::file_copy(path = file, new_path = destination, overwrite = TRUE)
 )
